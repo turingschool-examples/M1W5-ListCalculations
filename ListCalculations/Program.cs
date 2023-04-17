7a74149cfb8a58c2743edec1f4cb04d7e404f58b
Richard T <richard@turing.edu>
2023-04-17 12:36:14 -0400

    Add project files.

---
+++ ListCalculations/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ListCalculations
+{
+    public class Program
+    {
+        // Get the sum of the 4 smallest values in a user-submitted list
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a list of numbers, separated by a comma:");
+            var input = Console.ReadLine();
+
+            string[] splitInput = input.Split(',');
+
+            List<int> nums = new List<int>();
+
+            foreach (var item in splitInput)
+            {
+                nums.Add(Convert.ToInt32(item));
+            }
+
+            nums.Sort();
+
+            var smallNums = nums.GetRange(0, 4);
+
+            Console.WriteLine("The sum of the 4 smallest values is " + smallNums.Sum());
+        }
+    }
+}
